d291ae2a2cabae4d01f2aa602b76d19b04fcb712
Gabriel Malpica <gabrielmalpicacruz@gmail.com>
2025-04-14 13:14:54 -0500

    Se agregó mensaje cuando se conecte el bot a railway

--- src/app.ts
+++ src/app.ts
@@ -48,16 +48,16 @@ const flowValidation = addKeyword(EVENTS.WELCOME)
 
 const main = async () => {
     const adapterFlow = createFlow([flowDirector ,flowValidation ,flowPrincipal, menuTodero, menuCliente, menuAdministrador, menuServiciosGenerales, menuSalvavidas, menuSupervisor])
-    
+        
     const adapterProvider = createProvider(Provider)
     const adapterDB = new Database()
-
+    
     const { handleCtx, httpServer } = await createBot({
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
     })
-
+    
     adapterProvider.server.post(
         '/v1/messages',
         handleCtx(async (bot, req, res) => {
@@ -66,16 +66,16 @@ const main = async () => {
             return res.end('sended')
         })
     )
-
+    
     adapterProvider.server.post(
-        '/v1/register',
+            '/v1/register',
         handleCtx(async (bot, req, res) => {
             const { number, name } = req.body
             await bot.dispatch('REGISTER_FLOW', { from: number, name })
             return res.end('trigger')
         })
     )
-
+    
     adapterProvider.server.post(
         '/v1/samples',
         handleCtx(async (bot, req, res) => {
@@ -84,20 +84,21 @@ const main = async () => {
             return res.end('trigger')
         })
     )
-
+    
     adapterProvider.server.post(
         '/v1/blacklist',
-        handleCtx(async (bot, req, res) => {
+           handleCtx(async (bot, req, res) => {
             const { number, intent } = req.body
             if (intent === 'remove') bot.blacklist.remove(number)
             if (intent === 'add') bot.blacklist.add(number)
-
+    
             res.writeHead(200, { 'Content-Type': 'application/json' })
             return res.end(JSON.stringify({ status: 'ok', number, intent }))
         })
     )
-
+    
     httpServer(+PORT)
+    console.log(`✅ Bot iniciado y escuchando en el puerto ${PORT}`)
 }
-
+    
 main()

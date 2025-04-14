ec62a7b4df44d8fbb218aa2464b0fc7d537dee29
Gabriel Malpica <gabrielmalpicacruz@gmail.com>
2025-04-14 13:25:44 -0500

    Prueba mensaje en consola

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,12 @@ const PORT = process.env.PORT ?? 3008
 
 const bienvenida = leerArchivo('./mensajes/bienvenida.txt');
 
+const flowAlgo = addKeyword(EVENTS.WELCOME)
+    .addAction(async (ctx, ctxFn) =>{
+        console.log(ctx.body)
+        ctxFn.gotoFlow(flowValidation);
+    })
+
 const flowPrincipal = addKeyword<Provider, Database>(EVENTS.ACTION)
     .addAnswer(bienvenida, {
         media: './imgs/controlito.jpg'
@@ -47,17 +53,17 @@ const flowValidation = addKeyword(EVENTS.WELCOME)
     })
 
 const main = async () => {
-    const adapterFlow = createFlow([flowDirector ,flowValidation ,flowPrincipal, menuTodero, menuCliente, menuAdministrador, menuServiciosGenerales, menuSalvavidas, menuSupervisor])
-        
+    const adapterFlow = createFlow([flowAlgo,flowDirector ,flowValidation ,flowPrincipal, menuTodero, menuCliente, menuAdministrador, menuServiciosGenerales, menuSalvavidas, menuSupervisor])
+    
     const adapterProvider = createProvider(Provider)
     const adapterDB = new Database()
-    
+
     const { handleCtx, httpServer } = await createBot({
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
     })
-    
+
     adapterProvider.server.post(
         '/v1/messages',
         handleCtx(async (bot, req, res) => {
@@ -66,16 +72,16 @@ const main = async () => {
             return res.end('sended')
         })
     )
-    
+
     adapterProvider.server.post(
-            '/v1/register',
+        '/v1/register',
         handleCtx(async (bot, req, res) => {
             const { number, name } = req.body
             await bot.dispatch('REGISTER_FLOW', { from: number, name })
             return res.end('trigger')
         })
     )
-    
+
     adapterProvider.server.post(
         '/v1/samples',
         handleCtx(async (bot, req, res) => {
@@ -84,21 +90,20 @@ const main = async () => {
             return res.end('trigger')
         })
     )
-    
+
     adapterProvider.server.post(
         '/v1/blacklist',
-           handleCtx(async (bot, req, res) => {
+        handleCtx(async (bot, req, res) => {
             const { number, intent } = req.body
             if (intent === 'remove') bot.blacklist.remove(number)
             if (intent === 'add') bot.blacklist.add(number)
-    
+
             res.writeHead(200, { 'Content-Type': 'application/json' })
             return res.end(JSON.stringify({ status: 'ok', number, intent }))
         })
     )
-    
+
     httpServer(+PORT)
-    console.log(`✅ Bot iniciado y escuchando en el puerto ${PORT}`)
 }
-    
+
 main()
